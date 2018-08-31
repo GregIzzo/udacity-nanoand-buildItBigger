3bfde586ecad6560af341e3ec7466ca24b02263b
Greg Izzo <greg.izzo@gmail.com>
2018-08-31 13:30:19 -0400

    feat: Added javaJokes Java Library (module)
    Added javaJoke module (Java library). Connected it and revised app MainActivity to call the 'getJoke()' method

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.android.javajokes.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker myJoker = new Joker();
+        String joke = myJoker.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
